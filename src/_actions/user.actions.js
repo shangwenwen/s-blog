8fef654be0466625ae79cfa22d3704d9f87275a7
shangwenwen <1967553097@qq.com>
2018-05-16 17:30:59 +0800

    all

--- src/_actions/user.actions.js
+++ src/_actions/user.actions.js
@@ -27,26 +27,11 @@ function login(username, password) {
     }
   };
 
-  function request(user) {
-    return {
-      type: userConstants.LOGIN_REQUEST,
-      user
-    };
-  }
+  function request(user) { return { type: userConstants.LOGIN_REQUEST, user } }
 
-  function success(user) {
-    return {
-      type: userConstants.LOGIN_SUCCESS,
-      user
-    };
-  }
+  function success(user) { return { type: userConstants.LOGIN_SUCCESS, user } }
 
-  function failure(error) {
-    return {
-      type: userConstants.LOGIN_FAILURE,
-      error
-    };
-  }
+  function failure(error) { return { type: userConstants.LOGIN_FAILURE, error } }
 }
 
 function logout() {
