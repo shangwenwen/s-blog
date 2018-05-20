e6eae3d2bccbba2cc8ed3ecdc0be0698b36ea0e8
shangwenwen <1967553097@qq.com>
2018-05-20 11:07:46 +0800

    all

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Route, Switch, withRouter } from 'react-router-dom'
+import { Route, Switch, withRouter, Link, NavLink } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
 
 // components & containers
@@ -15,6 +15,11 @@ class AppContainer extends Component {
   render() {
     return (
       <div className="main">
+        <div>
+          <NavLink activeClassName="current" exact to="/">home</NavLink>
+          <NavLink activeClassName="current" to="/">article</NavLink>
+          <NavLink activeClassName="current" to="/">about</NavLink>
+        </div>
         <div className="container">
           <Switch>
             <Route exact path="/" component={HomeContainer} />
